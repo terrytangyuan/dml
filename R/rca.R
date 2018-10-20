d96a7c1ec20dbe1139dcd8cd6cea36c2d7d58bf7
terrytangyuan <terrytangyuan@gmail.com>
2018-10-20 12:22:39 -0400

    Fix RCA example

--- R/rca.R
+++ R/rca.R
@@ -21,7 +21,11 @@
 #'               Each vector represents a chunklet, the elements
 #'               in the vectors indicate where the samples locate
 #'               in \code{x}. See examples for more information.
-#'
+#' @param useD optional. When not given, RCA is done in the
+#' original dimension and \code{B} is full rank. When \code{useD} is given,
+#' RCA is preceded by constraints based LDA which reduces
+#' the dimension to \code{useD}. \code{B} in this case is of rank \code{useD}.
+#' 
 #' @return list of the RCA results:
 #' \item{B}{The RCA suggested Mahalanobis matrix.
 #'          Distances between data points x1, x2 should be
@@ -65,102 +69,128 @@
 #'
 #' @examples
 #' \dontrun{
-#' # generate synthetic Gaussian data
-#' library("MASS")
+#' library("MASS") # generate synthetic multivariate normal data
+#' set.seed(42)
+#' k <- 100L # sample size of each class
+#' n <- 3L # specify how many classes
+#' N <- k * n # total sample size
+#' x1 <- mvrnorm(k, mu = c(-16, 8), matrix(c(15, 1, 2, 10), ncol = 2))
+#' x2 <- mvrnorm(k, mu = c(0, 0), matrix(c(15, 1, 2, 10), ncol = 2))
+#' x3 <- mvrnorm(k, mu = c(16, -8), matrix(c(15, 1, 2, 10), ncol = 2))
+#' x <- as.data.frame(rbind(x1, x2, x3)) # predictors
+#' y <- gl(n, k) # response
 #'
-#' k <- 100 # sample size of each class
-#' n <- 3 # number of classes
-#' N <- k * n # total number of samples
-#'
-#' set.seed(1234)
-#' x1 <- mvrnorm(k, mu = c(-10, 6), matrix(c(10, 4, 4, 10), ncol = 2))
-#' x2 <- mvrnorm(k, mu = c(0, 0), matrix(c(10, 4, 4, 10), ncol = 2))
-#' x3 <- mvrnorm(k, mu = c(10, -6), matrix(c(10, 4, 4, 10), ncol = 2))
-#' x <- as.data.frame(rbind(x1, x2, x3))
-#' x$V3 <- gl(n, k)
-#'
-#' # The fully labeled data set with 3 classes
-#' plot(x$V1, x$V2,
-#'   bg = c("#E41A1C", "#377EB8", "#4DAF4A")[x$V3],
-#'   pch = c(rep(22, k), rep(21, k), rep(25, k))
+#' # fully labeled data set with 3 classes
+#' # need to use a line in 2D to classify
+#' plot(x[, 1L], x[, 2L],
+#'   bg = c("#E41A1C", "#377EB8", "#4DAF4A")[y],
+#'   pch = rep(c(22, 21, 25), each = k)
 #' )
-#' Sys.sleep(3)
+#' abline(a = -10, b = 1, lty = 2)
+#' abline(a = 12, b = 1, lty = 2)
 #'
-#' # The same data unlabeled; clearly the class structure is less evident
-#' plot(x$V1, x$V2)
-#' Sys.sleep(3)
+#' # generate synthetic chunklets
+#' chunks <- vector("list", 300)
+#' for (i in 1:100) chunks[[i]] <- sample(1L:100L, 10L)
+#' for (i in 101:200) chunks[[i]] <- sample(101L:200L, 10L)
+#' for (i in 201:300) chunks[[i]] <- sample(201L:300L, 10L)
 #'
-#' chunk1 <- sample(1:100, 5)
-#' chunk2 <- sample(setdiff(1:100, chunk1), 5)
-#' chunk3 <- sample(101:200, 5)
-#' chunk4 <- sample(setdiff(101:200, chunk3), 5)
-#' chunk5 <- sample(201:300, 5)
-#' chks <- x[c(chunk1, chunk2, chunk3, chunk4, chunk5), ]
-#' chunks <- list(chunk1, chunk2, chunk3, chunk4, chunk5)
+#' chks <- x[unlist(chunks), ]
 #'
-#' # The chunklets provided to the RCA algorithm
-#' plot(chks$V1, chks$V2,
-#'   col = rep(c(
-#'     "#E41A1C", "#377EB8",
-#'     "#4DAF4A", "#984EA3", "#FF7F00"
-#'   ), each = 5),
-#'   pch = rep(0:4, each = 5), ylim = c(-15, 15)
-#' )
-#' Sys.sleep(3)
+#' # make "chunklet" vector to feed the chunks argument
+#' chunksvec <- rep(-1L, nrow(x))
+#' for (i in 1L:length(chunks)) {
+#'   for (j in 1L:length(chunks[[i]])) {
+#'     chunksvec[chunks[[i]][j]] <- i
+#'   }
+#' }
 #'
-#' # Whitening transformation applied to the  chunklets
-#' chkTransformed <- as.matrix(chks[, 1:2]) %*% rca(x[, 1:2], chunks)$A
+#' # relevant component analysis
+#' rcs <- rca(x, chunksvec)
 #'
-#' plot(chkTransformed[, 1], chkTransformed[, 2],
-#'   col = rep(c(
-#'     "#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00"
-#'   ), each = 5),
-#'   pch = rep(0:4, each = 5), ylim = c(-15, 15)
-#' )
-#' Sys.sleep(3)
+#' # learned transformation of the data
+#' rcs$A
 #'
-#' # The origin data after applying the RCA transformation
-#' plot(rca(x[, 1:2], chunks)$newX[, 1], rca(x[, 1:2], chunks)$newX[, 2],
+#' # learned Mahalanobis distance metric
+#' rcs$B
+#'
+#' # whitening transformation applied to the chunklets
+#' chkTransformed <- as.matrix(chks) %*% rcs$A
+#'
+#' # original data after applying RCA transformation
+#' # easier to classify - using only horizontal lines
+#' xnew <- rcs$newX
+#' plot(xnew[, 1L], xnew[, 2L],
 #'   bg = c("#E41A1C", "#377EB8", "#4DAF4A")[gl(n, k)],
 #'   pch = c(rep(22, k), rep(21, k), rep(25, k))
 #' )
-#'
-#' # The RCA suggested transformation of the data, dimensionality reduced
-#' rca(x[, 1:2], chunks)$A
-#'
-#' # The RCA suggested Mahalanobis matrix
-#' rca(x[, 1:2], chunks)$B}
+#' abline(a = -15, b = 0, lty = 2)
+#' abline(a = 16, b = 0, lty = 2)
+#' }
 
-rca <- function(x, chunks) {
-  chunkNum <- length(chunks)
-  chunkDf <- vector("list", chunkNum)
-  p <- length(unlist(chunks))
-
-  for (i in 1:chunkNum) {
-    chunkDf[[i]] <- as.matrix(x[chunks[[i]], ])
+rca <- function(x, chunks, useD = NULL) {
+  n <- nrow(x)
+  d <- ncol(x)
+  if(is.null(useD)) useD = d
+  
+  # subtract the mean
+  TM <- colMeans(x)
+  x <- x - (matrix(1L, n, 1L) %*% TM)
+  
+  # compute chunklet means and center data
+  S <- max(chunks)
+  
+  Cdata <- matrix()
+  AllInds <- vector()
+  M <- matrix(NA, nrow = S, ncol = d)
+  tmp <- vector('list', S)
+  
+  for (i in 1L:S) {
+    inds <- which(chunks == i)
+    M[i, ] <- colMeans(x[inds, ])
+    tmp[[i]] <- x[inds, ] - matrix(1L, length(inds), 1L) %*% M[i, ]
+    Cdata <- do.call(rbind, tmp)
+    AllInds <- c(AllInds, inds)
   }
-
-  chunkMean <- lapply(chunkDf, colMeans)
-
-  for (i in 1:chunkNum) {
-    chunkDf[[i]] <- chunkDf[[i]] - chunkMean[[i]]
+  
+  # Compute inner covariance matrix
+  InnerCov <- cov(Cdata) * ((nrow(Cdata) - 1L) / nrow(Cdata))
+  
+  # Optional cFLD: find optimal projection: min | A S_w A^t | / | A S_t A^t |
+  
+  if (useD < d) {
+    # Compute total covariance using only chunkleted points
+    TotalCov <- cov(x[AllInds, ])
+    # TotalCov = cov(x)  # Compute total covariance using all the data.
+    # More accurate but may lead to PSD problems
+    tmp <- eigen(solve(TotalCov) %*% InnerCov)
+    D <- tmp$values
+    V <- tmp$vectors
+    # reorder the vectors in descending order
+    V <- V[ , ncol(V):1L]
+    # A is the cFLD transformation
+    # Acts on row data by multiplication from the right
+    A <- V[ , 1L:useD]
+    InnerCov <- t(A) %*% InnerCov %*% A
+  } else {
+    A <- diag(d)
   }
-
-  cData <- do.call(rbind, chunkDf) # calc inner covariance matrix and normalize
-  innerCov <- cov(cData) * ((nrow(cData) - 1) / nrow(cData))
-
-  for (i in 1:chunkNum) {
-    chunkDf[[i]] <- t(chunkDf[[i]]) %*% chunkDf[[i]]
-  }
-
-  hatC <- Reduce("+", chunkDf) / p # Reduce() do the sum of matrices in a list
-
-  B <- solve(hatC) # raw mahalanobis metric
-
-  A <- diag(ncol(x))
-  A <- A %*% (innerCov %^% (-0.5)) # whitening transformation matrix
-
-  newX <- as.matrix(x) %*% A # original data transformed
+  
+  # RCA: whiten the data w.r.t the inner covariance matrix
+  tmp <- svd(InnerCov)
+  U1 <- tmp$u
+  S1 <- diag(tmp$d)
+  V1 <- tmp$v
+  
+  # Operate from the right on row vectors
+  A <- as.matrix(A %*% (U1 %*% S1^(0.5)))
+  
+  # The total mean subtracted is re-added before transformation.
+  # This operation is not required for distance computations,
+  # as it only adds a constant to all points
+  newX <- as.matrix(x + matrix(1L, n, 1L) %*% TM) %*% A
+  
+  B <- A %*% t(A)
 
   out <- list("B" = B, "A" = A, "newX" = newX)
 
