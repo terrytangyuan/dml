55aaf65d013131c244e48a86120f5552594d10c4
Nan Xiao <road2stat@gmail.com>
2018-10-19 22:39:10 -0400

    add dml-package

---
+++ R/dml-package.R
@@ -0,0 +1,17 @@
+#' Distance Metric Learning in R
+#'
+#' Solving distance metric learning problems with R.
+#'
+#' \tabular{ll}{
+#' Package: \tab dml\cr
+#' Type: \tab Package\cr
+#' License: \tab MIT}
+#'
+#' @name dml-package
+#' @docType package
+#' @author
+#' Yuan Tang <\email{terrytangyuan@@gmail.com}>
+#' Tao Gao <\email{joegaotao@@gmail.com}>
+#' Nan Xiao <\email{me@@nanx.me}>
+#'
+NULL
